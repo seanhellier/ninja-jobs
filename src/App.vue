17c0899086aca5b6662de958cfb338311ea383f3
Sean Leif Hellier <sean@newtropolis.com>
2021-06-27 17:08:03 -0400

    lesson 49

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">Home</router-link>
+    <router-link :to="{ name: 'About' }">About</router-link>
     <router-link :to="{ name: 'Jobs' }">Jobs</router-link>
   </div>
   <router-view/>
@@ -14,18 +14,21 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  max-width: 960px;
+  margin: 0 auto;
 }
-
 #nav {
   padding: 30px;
 }
-
 #nav a {
   font-weight: bold;
   color: #2c3e50;
+  text-decoration: none;
+  padding: 10px;
+  border-radius: 4px;
 }
-
 #nav a.router-link-exact-active {
-  color: #42b983;
+  color: white;
+  background: crimson;
 }
 </style>
