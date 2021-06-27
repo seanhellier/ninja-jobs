295e1cdb468a855556355cb4ca3b2f3551d68a19
Sean Leif Hellier <sean@newtropolis.com>
2021-06-27 15:13:42 -0400

    lesson 48

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,14 @@
 <template>
   <div class="home">
-    <p>lorem100</p>
-     <p>lorem100</p>
+    <h1>Homepage</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam, praesentium dolorum libero reprehenderit fugiat exercitationem necessitatibus! Incidunt, eum. Aperiam facere magnam consectetur omnis molestiae vero. Placeat illum animi deserunt in illo, amet perspiciatis corrupti ex quo aut, ducimus earum sapiente! Odio pariatur facere voluptatibus libero magnam possimus quo voluptatum nobis.</p>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam, praesentium dolorum libero reprehenderit fugiat exercitationem necessitatibus! Incidunt, eum. Aperiam facere magnam consectetur omnis molestiae vero. Placeat illum animi deserunt in illo, amet perspiciatis corrupti ex quo aut, ducimus earum sapiente! Odio pariatur facere voluptatibus libero magnam possimus quo voluptatum nobis.</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-
-
 export default {
   name: 'Home',
-  components: {
-
-  }
+  components: {}
 }
 </script>
