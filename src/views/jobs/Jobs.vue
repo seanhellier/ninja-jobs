be8f546cd5980962900594a183055ae15464d7ad
Sean Leif Hellier <sean@newtropolis.com>
2021-06-28 15:38:07 -0400

    lesson 56

--- src/views/jobs/Jobs.vue
+++ src/views/jobs/Jobs.vue
@@ -1,9 +1,14 @@
 <template>
   <h1>Jobs</h1>
-  <div v-for="job in jobs" :key="job.id" class="job">
-    <router-link :to="{name: 'JobDetails', params: { id: job.id }}">
-      <h2>{{ job.title }}</h2>
-    </router-link>
+  <div v-if="jobs.length">
+    <div v-for="job in jobs" :key="job.id" class="job">
+      <router-link :to="{name: 'JobDetails', params: { id: job.id }}">
+        <h2>{{ job.title }}</h2>
+      </router-link>
+    </div>
+  </div>
+  <div v-else>
+    <p>Loading jobs...</p>
   </div>
 </template>
 
@@ -15,10 +20,10 @@ export default {
     }
   },
   mounted() {
-    fetch("http://localhost:3000/jobs")
-    .then(res => res.json())
-    .then(data => this.jobs = data)
-    .catch(err => console.log(err.message))
+    fetch('http://localhost:3000/jobs')
+      .then(res => res.json())
+      .then(data => this.jobs = data)
+      .catch(err => console.log(err.message))
   }
 }
 </script>
